fa259d43cd2d5f2666cb171ebace22a8caafb669
Rodrigo <rmmaf@cin.ufpe.br>
2017-06-19 13:45:11 -0300

    problemas com css(flex)

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-function inputFocus(i){
-    if(i.value==i.defaultValue){ i.value=""; i.style.color="#000"; }
-}
-function inputBlur(i){
-    if(i.value==""){ i.value=i.defaultValue; i.style.color="#888"; }
-}
